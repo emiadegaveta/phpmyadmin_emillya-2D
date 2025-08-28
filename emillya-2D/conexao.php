95ad442ee3c701abc8efa835a3316c8a0db4838d
emillyaa <emilly.nascimento02@escola.pr.gov.br>
2025-08-28 10:46:38 -0300

    Update conexao.php

--- emillya-2D/conexao.php
+++ emillya-2D/conexao.php
@@ -2,3 +2,15 @@
 
 $host="localhost";
 $usuario="root";
+$email="";
+$banco="mural_de_produtos";
+
+$conexao= mysqli_connect($host, $usuario, $email, $banco);
+
+if (!$conexao) {
+    die("Erro ao conectar: " . mysqli_connect_error());
+}
+
+mysqli_set_charset($conexao, "utf8");
+?>
+ 
